d80a9363b6c8f920f923746d63e08ac89400f818
RabbitHu <yuanxiaodidl@163.com>
2022-04-18 10:42:46 -0700

    add main

---
+++ mpm/main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import torch
+from functional import *
+
+
+def main():
+    print("hi")
+
+
+if __name__ == '__main__':
+    main()
