b5c571f8a85afb6c8e3ce68d13e1b8b9981241fe
RabbitHu <yuanxiaodidl@163.com>
2022-05-02 11:59:45 -0700

    add svd3x3_backward

--- pytorch_svd3x3/functional.py
+++ pytorch_svd3x3/functional.py
@@ -12,22 +12,28 @@ __all__ = ['svd3x3']
 
 class Svd3x3(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, inputs):
+    def forward(ctx, inputs: torch.Tensor):
         '''
             input: [N, 3, 3]
         '''
         n = inputs.shape[0]
-        inputs = inputs.permute(1, 2, 0).contiguous()
+        inputs = inputs.permute(1, 2, 0).contiguous() # [3, 3, N]
         results = lib.svd3x3_forward(inputs) # [21, N]
         results = results.permute(1, 0).contiguous()
         U, S, V = results.split([9, 3, 9], dim=-1)
         U = U.view(n, 3, 3)
         V = V.view(n, 3, 3)
 
+        ctx.save_for_backward(inputs, U, S, V)
+
         return U, S, V
 
     @staticmethod
-    def backward(ctx, grad_h, grad_cell):
-        raise NotImplementedError
+    def backward(ctx, grad_u: torch.Tensor, grad_s: torch.Tensor, grad_v: torch.Tensor):
+        A, U, S, V = ctx.saved_tensors
+        grad_out: torch.Tensor = lib.svd3x3_backward(
+            [grad_u, grad_s, grad_v], A, True, True, U.to(A.dtype), S.to(A.dtype), V.to(A.dtype)
+        )
+        return grad_out
 
 svd3x3 = Svd3x3.apply
--- test_svd3x3.py
+++ test_svd3x3.py
@@ -1,9 +1,35 @@
+from pytorch_svd3x3 import svd3x3
 import torch
-from pytorch_svd3x3 import *
 
-x = torch.randn((2, 3, 3)).cuda()
-print(x)
-print("=========== svd3x3 ===========")
-print('\n'.join(map(str, svd3x3(x))))
-print("=========== torch.linalg.svd ===========")
-print(torch.linalg.svd(x))
+A_torch = torch.randn(2, 3, 3).cuda()
+A_33 = A_torch.clone()
+A_torch.requires_grad_()
+A_33.requires_grad_()
+print(A_torch.is_leaf, A_33.is_leaf)
+U_torch, sigma_torch, V_torch = torch.svd(A_torch)
+U_33, sigma_33, V_33 = svd3x3(A_33)
+
+print("====== U ======")
+print("U_torch:", U_torch)
+print("U_33   :", U_33)
+
+print("====== sigma ======")
+print("sigma_torch:", sigma_torch)
+print("sigma_33   :", sigma_33)
+
+print("====== V ======")
+print("V_torch:", V_torch)
+print("V_33   :", V_33)
+
+
+print("====== A.grad ======")
+
+U_c, sigma_c, V_c = torch.randn(2, 3, 3).cuda(), torch.randn(2, 3).cuda(), torch.randn(2, 3, 3).cuda()
+
+loss_torch = (U_c * U_torch**2).sum() + (sigma_c * sigma_torch**2).sum() + (V_c * V_torch**2).sum()
+loss_torch.backward()
+print("loss_torch:", A_torch.grad)
+
+loss_33 = (U_c * U_33**2).sum() + (sigma_c * sigma_33**2).sum() + (V_c * V_33**2).sum()
+loss_33.backward()
+print("loss_torch:", A_33.grad)
