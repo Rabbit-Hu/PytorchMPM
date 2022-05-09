23ae2ce7eab142e660ea89a41ac42fea7b882e6f
RabbitHu <yuanxiaodidl@163.com>
2022-05-09 15:35:55 -0700

    save

--- learnable/learn_Psi/scripts/gen_full_traj.py
+++ learnable/learn_Psi/scripts/gen_full_traj.py
@@ -15,7 +15,7 @@ from functional import avg_voxelize, mpm_p2g, mpm_g2p
 # from torch_batch_svd import svd as fast_svd
 from pytorch_svd3x3 import svd3x3
 
-from model.model import MPMModel
+from model.model import MPMModelLearnedPhi
 
 import random
 random.seed(20010313)
@@ -26,7 +26,9 @@ torch.manual_seed(20010313)
 def main(args):
     import taichi as ti # only for GUI (TODO: re-implement GUI to remove dependence on taichi)
     ti.init(arch=ti.cpu)
-    gui = ti.GUI("Taichi MLS-MPM-99", res=512, background_color=0x112F41)
+    gui_gt = ti.GUI("Pred", res=512, background_color=0x112F41)
+    gui_pred = ti.GUI("Pred", res=512, background_color=0x112F41)
+    gui_guess = ti.GUI("Guess", res=512, background_color=0x112F41)
 
     device = torch.device('cuda:0')
 
@@ -48,33 +50,39 @@ def main(args):
     video_dir = os.path.join(log_dir, 'video')
     os.makedirs(video_dir, exist_ok=True)
 
-    x, v, C, F = data_dict['x_traj'][args.start_frame].to(device), data_dict['v_traj'][args.start_frame].to(device), \
-                 data_dict['C_traj'][args.start_frame].to(device), data_dict['F_traj'][args.start_frame].to(device)
-    material = torch.ones((len(x),), dtype=torch.int, device=device)
-    Jp = torch.ones((len(x),), dtype=torch.float, device=device)
+    x_traj = data_dict['x_traj']
+    x_phi, v_phi, C_phi, F_phi = data_dict['x_traj'][args.start_frame].to(device), data_dict['v_traj'][args.start_frame].to(device), \
+                                 data_dict['C_traj'][args.start_frame].to(device), data_dict['F_traj'][args.start_frame].to(device)
+    x_enu, v_enu, C_enu, F_enu = x_phi.clone(), v_phi.clone(), C_phi.clone(), F_phi.clone()
+    material = torch.ones((len(x_phi),), dtype=torch.int, device=device)
+    Jp = torch.ones((len(x_phi),), dtype=torch.float, device=device)
 
-    mpm_model = MPMModel(*mpm_model_init_params).to(device)
-    mpm_model.load_state_dict(torch.load(args.model_path))
+    mpm_model_learned_phi = MPMModelLearnedPhi(*mpm_model_init_params).to(device)
+    mpm_model_learned_phi.load_state_dict(torch.load(args.model_path))
+    mpm_model_guess = MPMModelLearnedPhi(*mpm_model_init_params, learn_phi=False).to(device)
 
-    for frame_i in range(args.start_frame, traj_len):
-        for s in range(n_iter_per_frame):
-            x, v, C, F, material, Jp = mpm_model(x, v, C, F, material, Jp, E, nu)
+    with torch.no_grad():
+        for frame_i in range(args.start_frame, traj_len):
+            print(f"frame_i = {frame_i}")
+            for s in range(n_iter_per_frame):
+                x_phi, v_phi, C_phi, F_phi, material, Jp = mpm_model_learned_phi(x_phi, v_phi, C_phi, F_phi, material, Jp, E, nu)
+                x_enu, v_enu, C_enu, F_enu, material, Jp = mpm_model_learned_phi(x_enu, v_enu, C_enu, F_enu, material, Jp, E, nu)
 
-        # gui.circles(x_start.detach().cpu().numpy(), radius=1.5, color=0x068587)
-        gui.circles(x.detach().cpu().numpy(), radius=1.5, color=0xED553B)
-        # gui.circles(x_traj[-1].detach().cpu().numpy(), radius=1.5, color=0xEEEEF0)
-        filename = os.path.join(video_dir, f"{frame_i:06d}.png")
-        # NOTE: use ffmpeg to convert saved frames to video:
-        #       ffmpeg -framerate 30 -pattern_type glob -i '*.png' -vcodec mpeg4 -vb 20M out.mov
-        gui.show(filename) # Change to gui.show(f'{frame:06d}.png') to write images to disk
+            gui_gt.circles(x_traj[frame_i].numpy(), radius=1.5, color=0xEEEEF0)
+            gui_pred.circles(x_phi.detach().cpu().numpy(), radius=1.5, color=0xED553B)
+            gui_guess.circles(x_enu.detach().cpu().numpy(), radius=1.5, color=0x068587)
+            filename = os.path.join(video_dir, f"{frame_i:06d}.png")
+            # NOTE: use ffmpeg to convert saved frames to video:
+            #       ffmpeg -framerate 30 -pattern_type glob -i '*.png' -vcodec mpeg4 -vb 20M out.mov
+            gui_pred.show(os.path.join(video_dir, f"{frame_i:06d}.png")) # Change to gui.show(f'{frame:06d}.png') to write images to disk
         
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--traj_path', type=str, default='learnable/learn_Psi/data/jelly/traj_0000')
-    parser.add_argument('--start_frame', type=int, default=0)
-    parser.add_argument('--model_path', type=str, default='learnable/learn_Psi/log/traj_0000_clip_0000/model/checkpoint_0119_loss_19.69.pth')
+    parser.add_argument('--start_frame', type=int, default=1)
+    parser.add_argument('--model_path', type=str, default='learnable/learn_Psi/log/exp/traj_0000_clip_0000/model/checkpoint_0199_loss_12.39.pth')
     args = parser.parse_args()
     print(args)
 
