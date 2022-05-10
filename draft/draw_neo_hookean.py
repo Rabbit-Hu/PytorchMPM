7af3455977bc0378cbaeb96e87f9d9a21de013e5
RabbitHu <yuanxiaodidl@163.com>
2022-05-10 14:04:23 -0700

    save

---
+++ draft/draw_neo_hookean.py
@@ -0,0 +1,6 @@
+import numpy
+import matplotlib.pyplot as plt
+
+
+def neo_hookean(F, nu, la):
+    
--- learnable/learn_Psi/scripts/train.py
+++ learnable/learn_Psi/scripts/train.py
@@ -16,6 +16,7 @@ from functional import avg_voxelize, mpm_p2g, mpm_g2p
 from pytorch_svd3x3 import svd3x3
 
 from model.model import MPMModelLearnedPhi
+from datasets.jelly_v2_dataset import JellyV2Dataset
 
 import random
 random.seed(20010313)
@@ -23,14 +24,63 @@ np.random.seed(20010313)
 torch.manual_seed(20010313)
 
 
+
+
 def main(args):
-    pass# TODO
+    device = torch.device('cuda:0')
+
+    train_dataset = JellyV2Dataset('learnable/learn_Psi/data/jelly_v2/config_0000', split='train', clip_len=args.clip_len)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+
+    config_dict = train_dataset.config_dict
+    n_grid = config_dict['n_grid']
+    dx = 1 / n_grid
+    dt = config_dict['dt']
+    frame_dt = config_dict['frame_dt']
+    n_iter_per_frame = int(frame_dt / dt + 0.5)
+    p_vol, p_rho = config_dict['p_vol'], config_dict['p_rho']
+    gravity = config_dict['gravity']
+    E_range = config_dict['E_range']
+    nu_range = config_dict['nu_range']
+
+    # log_dir = os.path.join('learnable/learn_Psi/log', args.exp_name, f'{traj_name}_clip_{clip_idx:04d}')
+
+    mpm_model = MPMModelLearnedPhi(2, n_grid, dx, dt, p_vol, p_rho, gravity)
+    optimizer = torch.optim.SGD(mpm_model.parameters(), lr=args.Psi_lr)
+
+    criterion = nn.MSELoss()
+    
+    for epoch in range(args.n_epoch):
+        for sample_id, (x_traj, v_traj, C_traj, F_traj) in enumerate(train_loader):
+            x_traj, v_traj, C_traj, F_traj = x_traj.squeeze(0).to(device), v_traj.squeeze(0).to(device), \
+                                                C_traj.squeeze(0).to(device), F_traj.squeeze(0).to(device)
+            ## x_traj [clip_len+1, n_particles, 2]
+            material = torch.ones((x_traj.shape[1],), dtype=torch.int, device=device) # [n_particles,]
+            Jp = torch.ones((x_traj.shape[1],), dtype=torch.float, device=device) # [n_particles,]
+            
+            loss = 0
+            x_scale = 1e3
+            x, v, C, F = x_traj[0], v_traj[0], C_traj[0], F_traj[0]
+            for clip_frame in range(1, clip_len + 1):
+                for s in range(n_iter_per_frame):
+                    x, v, C, F, material, Jp = mpm_model(x, v, C, F, material, Jp, E, nu)
+                if args.multi_frame:
+                    loss += criterion(x * x_scale, x_traj[clip_frame] * x_scale)
+            if not args.multi_frame:
+                loss = criterion(x * x_scale, x_traj[-1] * x_scale)
+            else:
+                loss /= clip_len
+            
+            
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str, default='train')
     parser.add_argument('--data_dir', type=str, default='/root/Concept/PytorchMPM/learnable/learn_Psi/data/jelly_v2/config_0000')
+    parser.add_argument('--n_epoch', type=int, default=1000, help='number of epoch')
+    parser.add_argument('--grad_accu', type=int, default=4, help='gradient accumulation step')
+    parser.add_argument('--Psi_lr', type=int, default=3e-2, help='learning rate for the neural network that outputs Psi')
     parser.add_argument('--learn_F', action='store_true')
     parser.add_argument('--learn_C', action='store_true')
     parser.add_argument('--clip_len', type=int, default=10, help='number of frames in the trajectory clip')
