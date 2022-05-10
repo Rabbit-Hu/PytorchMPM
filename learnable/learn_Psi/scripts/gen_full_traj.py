9e8d5663552d576d1efd768a1935cc4ee5078ef6
RabbitHu <yuanxiaodidl@163.com>
2022-05-09 17:08:27 -0700

    save

--- learnable/learn_Psi/scripts/gen_full_traj.py
+++ learnable/learn_Psi/scripts/gen_full_traj.py
@@ -26,6 +26,7 @@ torch.manual_seed(20010313)
 def main(args):
     import taichi as ti # only for GUI (TODO: re-implement GUI to remove dependence on taichi)
     ti.init(arch=ti.cpu)
+    gui_all = ti.GUI("All", res=512, background_color=0x112F41)
     gui_gt = ti.GUI("Pred", res=512, background_color=0x112F41)
     gui_pred = ti.GUI("Pred", res=512, background_color=0x112F41)
     gui_guess = ti.GUI("Guess", res=512, background_color=0x112F41)
@@ -46,10 +47,12 @@ def main(args):
     E = torch.rand((1,), dtype=torch.float, device=device) * (E_range[1] - E_range[0]) + E_range[0]
     nu = torch.rand((1,), dtype=torch.float, device=device) * (nu_range[1] - nu_range[0]) + nu_range[0]
 
-    log_dir = os.path.join('/root/Concept/PytorchMPM/learnable/learn_Psi/log', f'{os.path.split(args.traj_path)[-1]}_gen')
+    log_dir = os.path.join('/root/Concept/PytorchMPM/learnable/learn_Psi/log', args.exp_name, f'{os.path.split(args.traj_path)[-1]}_gen')
+    video_all_dir = os.path.join(log_dir, 'video_all')
     video_gt_dir = os.path.join(log_dir, 'video_gt')
     video_pred_dir = os.path.join(log_dir, 'video_pred')
     video_guess_dir = os.path.join(log_dir, 'video_guess')
+    os.makedirs(video_all_dir, exist_ok=True)
     os.makedirs(video_gt_dir, exist_ok=True)
     os.makedirs(video_pred_dir, exist_ok=True)
     os.makedirs(video_guess_dir, exist_ok=True)
@@ -73,18 +76,23 @@ def main(args):
                 x_enu, v_enu, C_enu, F_enu, material, Jp = mpm_model_learned_phi(x_enu, v_enu, C_enu, F_enu, material, Jp, E, nu)
 
             gui_gt.circles(x_traj[frame_i].numpy(), radius=1.5, color=0xEEEEF0)
+            gui_all.circles(x_traj[frame_i].numpy(), radius=1.5, color=0xEEEEF0)
             gui_pred.circles(x_phi.detach().cpu().numpy(), radius=1.5, color=0xED553B)
+            gui_all.circles(x_phi.detach().cpu().numpy(), radius=1.5, color=0xED553B)
             gui_guess.circles(x_enu.detach().cpu().numpy(), radius=1.5, color=0x068587)
+            gui_all.circles(x_enu.detach().cpu().numpy(), radius=1.5, color=0x068587)
             # NOTE: use ffmpeg to convert saved frames to video:
             #       ffmpeg -framerate 30 -pattern_type glob -i '*.png' -vcodec mpeg4 -vb 20M out.mov
             gui_gt.show(os.path.join(video_gt_dir, f"{frame_i:06d}.png"))
             gui_pred.show(os.path.join(video_pred_dir, f"{frame_i:06d}.png"))
             gui_guess.show(os.path.join(video_guess_dir, f"{frame_i:06d}.png"))
+            gui_all.show(os.path.join(video_all_dir, f"{frame_i:06d}.png"))
         
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', type=str, default='exp')
     parser.add_argument('--traj_path', type=str, default='learnable/learn_Psi/data/jelly/traj_0000')
     parser.add_argument('--start_frame', type=int, default=1)
     parser.add_argument('--model_path', type=str, default='learnable/learn_Psi/log/exp/traj_0000_clip_0000/model/checkpoint_0199_loss_12.39.pth')
